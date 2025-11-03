71552c773a6dc4be46fac53fd8292423fdc02acf
jckinsey01 <jckinsey@ncsu.edu>
2025-11-03 15:06:13 -0500

    added the things in moodle

--- src/03-Textual-Analysis/utils/utils.js
+++ src/03-Textual-Analysis/utils/utils.js
@@ -1,3 +1,20 @@
+/** getUniquePropListBy()
+* Goal: Create a unique list of `x` property
+* in an array of objects.
+* @params
+* - arr: Array. Any array of objects.
+* - key: String. Desired property to isolate.
+* @return
+* - uniqList: Array. List of unique data values.
+**/
+export const getUniquePropListBy = (arr, key) => {
+const uniqueObjs = [...new Map(arr.map(item => [item[key], item])).values()]
+const uniqList = []
+for (const o of uniqueObjs) {
+uniqList.push(o[key])
+}
+return uniqList
+}
 import nlp from "compromise"
 import {utcParse,utcFormat} from "d3-time-format"
 
